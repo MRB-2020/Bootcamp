a1bea26e34ad8939168cffe600e40c517ae374b3
MarcioRB <66230883+MRB-2020@users.noreply.github.com>
2022-07-09 11:09:52 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,7 +1,8 @@
 ##Intro to Python
 #just going trhough some basic 
 
-print('hello world!!')
+print("hello world!!\nLet's lern a new skill\n")
+print('We can skip a line on a print') #\n creates a new line in Python
 
 
 
