998ded93cf7dd509d15a9e8ce8757ffa3b1753dc
MarcioRB <66230883+MRB-2020@users.noreply.github.com>
2022-07-09 11:09:02 -0400

    uodate

--- intro.py
+++ intro.py
@@ -4,4 +4,10 @@
 print('hello world!!')
 
 
+###List
+# List is the basic way to declare (create) variables in Python
+# Single name for a collection of values; can containe any type of values and containe different types in the same list
 
+fam = ['dad',1.85,'mother', 1.76,'son',1.60] # fam list has strings and floats
+
+print(fam)
